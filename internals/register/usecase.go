fa7ee935e990aa072220a52da0666695c2734370
fahmi <fahmi@localhost.localdomain>
2023-07-17 19:41:03 +0700

    feat(api): email verification

--- internals/register/usecase.go
+++ internals/register/usecase.go
@@ -4,11 +4,29 @@ import (
 	"e-course/domain"
 	email "e-course/pkg/mail/sendgrid"
 	"e-course/pkg/resp"
+	"time"
 )
 
 type registerUsecase struct {
 	userUsecase domain.UserUsecase
 	mail        email.Mail
+}
+
+// VerificationCode implements domain.RegisterUsecase.
+func (u *registerUsecase) VerificationCode(dto domain.VerificationEmailRequestBody) *resp.ErrorResp {
+	user, err := u.userUsecase.FindOneByCodeVerified(dto.CodeVerified)
+	if err != nil {
+		return err
+	}
+	timeNow := time.Now()
+	dataUpdateUser := domain.UserUpdateRequestBody{
+		EmailVerifiedAt: &timeNow,
+	}
+	_, err = u.userUsecase.UpdatePassword(int(user.ID), dataUpdateUser)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (u *registerUsecase) Register(dto domain.UserCreateRequestBody) *resp.ErrorResp {
